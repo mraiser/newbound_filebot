3620a94f2f6cac189c606e819b1b6691bc09cc71
mraiser <marc@raiser.us>
2022-01-09 11:36:34 -0500

    Return search results from remote file shares asynchronously over websocket.

--- data/filebot/rlrz/hx17/7f95/f639/rlrzhx177f95f6390y1812.js
+++ data/filebot/rlrz/hx17/7f95/f639/rlrzhx177f95f6390y1812.js
@@ -3,34 +3,71 @@ var ME = $('#'+me.UUID)[0];
 
 me.ready = function(){
   componentHandler.upgradeAllRegistered();
-  
+  me.peer = ME.DATA.peer;
   me.prefix = ME.DATA.peer ? '../peerbot/remote/'+ME.DATA.peer+'/' : '../';
   document.searchFiles = searchFiles;
 };
 
 function searchFiles(){
   $(ME).find('.filesearchresults').empty();
-  var query = $(ME).find('.filesearchquery').val();
-  var path = ME.DATA.path ? ME.DATA.path : '/';
-  var params = 'path='+encodeURIComponent(path)+'&query='+encodeURIComponent(query)+'&uuid='+encodeURIComponent(me.UUID);
+  
+    var url = document.URL;
+    url = url.substring(url.indexOf(':'));
+    url = url.substring(0, url.indexOf('/',3));
+    url = 'ws'+url+(typeof me.peer == 'string' ? '/peerbot/remote/'+me.peer+'/filebot/index.html' : '/filebot/index.html');
+
+    var connection = new WebSocket(url, ['newbound']);
+
+    connection.onopen = function(){
+      console.log('Web Socket to filebot open: '+url);
+
+
+  
+      var query = $(ME).find('.filesearchquery').val();
+      var path = ME.DATA.path ? ME.DATA.path : '/';
+      var uuid = guid(); //me.UUID;
+      var params = 'path='+encodeURIComponent(path)+'&query='+encodeURIComponent(query)+'&uuid='+encodeURIComponent(uuid);
+
+      var now = new Date().getTime();
+      json(me.prefix+'filebot/search', params, function(result){
+        var el = $(ME).find('.filesearchresults');
+        if (result.status == 'ok'){
+          if (!me.gotwsdata){
+            if (result.list.length>0)
+              for (var i in result.list)
+                addResultRow(result.list[i]);
+            else el.append('<i>No results found. </i>');
+          }
+          me.gotwsdata = false;
+          el.append('<i>Search done in '+(new Date().getTime()-now)+'ms</i>');
+        }
+        else el.append('<i>'+result.msg+'</i>');
+        connection.close();
+      }, ME.DATA.peer);
       
-  var now = new Date().getTime();
-  json(me.prefix+'filebot/search', params, function(result){
-    var el = $(ME).find('.filesearchresults');
-    if (!me.gotwsdata){
-      if (result.list.length>0)
-        for (var i in result.list)
-          addResultRow(result.list[i]);
-      else el.append('<i>No results found. </i>');
-    }
-    me.gotwsdata = false;
-    el.append('<i>Search done in '+(new Date().getTime()-now)+'ms</i>');
-  }, ME.DATA.peer);
+      
+    };
+
+    connection.onerror = function(error){
+      console.log('Web Socket to filebot error');
+      connection.close();
+    };
+
+    connection.onclose = function(error){
+      console.log('Web Socket to filebot close');
+    };
+
+    connection.onmessage = function(e){
+      var data = JSON.parse(e.data);
+      console.log(data)
+      me.wscb(data);
+    };
+
 }
 
 function addResultRow(filename){
   var el = $('<a class="ablock" target="_blank"/>');
-  el.prop('href', '../filebot/local'+filename);
+  el.prop('href', me.prefix+'filebot/local'+filename);
   el.text(filename);
   $(ME).find('.filesearchresults').append(el);
 }
